606f8304b405c708dfd41aa7487c74105b47179a
caokaiyan <cky951010@163.com>
2018-08-23 23:12:37 +0800

    提供config.example.php模板，不再跟踪confi.php

---
+++ config.example.php
@@ -0,0 +1,21 @@
+<?php
+
+/////////////////////////// 用户自定义配置 //////////////////////////
+
+// 博客名称
+define("BLOG_TITLE","CodekissYoung Blog");
+
+// markdown文件所在目录
+define("MD_ROOT","/home/cky/workspace/md/");
+
+// 博客首页加载的 markdown 文件
+define("DEFAULT_ARTICLE",MD_ROOT."link.md");
+
+// markdown目录下需要被跳过的文件夹
+$IGNORE_DIR = [".git","img"];
+
+/////////////////////////// Blog 系统配置 ///////////////////////////
+
+define("APP_ROOT",__DIR__);
+include_once 'function/common.php';   // 公共方法
+include_once 'function/autoload.php'; // 自动加载
