e3e1523a65e332b5cf244252e4f349774c8f7d7d
caokaiyan <cky951010@163.com>
2019-03-01 13:05:47 +0800

    调整

--- web/view/index.php
+++ web/view/index.php
@@ -2,6 +2,9 @@
 <div id="article">
     <nav id="main_category">
         <div id="main-category-content">
+			<div id="main-category-title">
+				<h2>目录</h2>
+			</div>
             <?=$category?>
         </div>
     </nav>
