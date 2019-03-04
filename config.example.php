7fd5162b6f2ac75705cd070f93e9ddb583242be0
caokaiyan <cky951010@163.com>
2019-03-04 20:47:10 +0800

    修改默认配置文件

--- config.example.php
+++ config.example.php
@@ -3,18 +3,18 @@
 /////////////////////////// 用户自定义配置 //////////////////////////
 
 // 博客名称
-define("BLOG_TITLE","CodekissYoung Blog");
+define("BLOG_TITLE","Simple Markdown Blog");
 
 // markdown文件所在目录
-define("MD_ROOT","/home/cky/workspace/md/");
+define("MD_ROOT","/home/user1/workspace/md/");
 
-// 博客首页加载的 markdown 文件
-define("DEFAULT_ARTICLE",MD_ROOT."link.md");
+// 博客首页加载的 markdown 文件,若网站首页需要固定展示某篇博客时，可设置
+// $DEFAULT_ARTICLE = MD_ROOT."android/安卓教程.md";
 
-// markdown目录下需要被跳过的文件夹
+// MD_ROOT 下需要被跳过的文件夹
 $IGNORE_DIR = [".git","img","android/java教程"];
 
-// markdown目录下需要被跳过的文件
+// MD_ROOT 下需要被跳过的文件
 $IGNORE_FILE = ["link","default","xxx_dir/xxx_dir/file_name"];
 
 // 博客的自定义链接
