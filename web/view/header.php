a2bccad30101daa09afae4070d4ad1fdbb282279
caokaiyan <cky951010@163.com>
2019-05-09 12:03:53 +0800

    更新jquery库到3.4.1

--- web/view/header.php
+++ web/view/header.php
@@ -9,7 +9,7 @@
         <link href="/css/github.css" rel="stylesheet">
         <link href="/css/github-markdown.css" rel="stylesheet">
         <link href="/css/common.css" rel="stylesheet"/>
-        <script src="/js/jquery-3.3.1.min.js"></script>
+        <script src="/js/jquery-3.4.1.min.js"></script>
         <script src="/js/highlight.pack.js"></script>
         <script src="/js/main.js"></script>
     </head>
